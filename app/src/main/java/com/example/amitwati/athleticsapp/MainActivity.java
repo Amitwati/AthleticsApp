326cf59c36363a9d3ee57bc53760f672fc96e7f9
Amitwati <amitwati69@gmail.com>
2017-11-07 12:37:56 +0200

    graph library test

--- app/src/main/java/com/example/amitwati/athleticsapp/MainActivity.java
+++ app/src/main/java/com/example/amitwati/athleticsapp/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.amitwati.athleticsapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.PieModel;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ValueLineChart mCubicValueLineChart = (ValueLineChart) findViewById(R.id.cubiclinechart);
+
+        ValueLineSeries series = new ValueLineSeries();
+        series.setColor(Color.RED);
+
+        for(int i=-10;i<50;i++) {
+            series.addPoint(new ValueLinePoint(String.valueOf(i), i*i*i));
+        }
+
+
+        mCubicValueLineChart.addSeries(series);
+        mCubicValueLineChart.startAnimation();
+
+        PieChart mPieChart = (PieChart) findViewById(R.id.piechart);
+
+        mPieChart.addPieSlice(new PieModel("Freetime", 15, Color.parseColor("#FE6DA8")));
+        mPieChart.addPieSlice(new PieModel("Sleep", 25, Color.parseColor("#56B7F1")));
+        mPieChart.addPieSlice(new PieModel("Work", 35, Color.parseColor("#CDA67F")));
+        mPieChart.addPieSlice(new PieModel("Eating", 9, Color.parseColor("#FED70E")));
+
+        mPieChart.startAnimation();
+
+
+
     }
 }
