2af1f82679d1a363c62e9d80798240ae3e1278fc
Amitwati <Amitwati69@gmail.com>
2017-11-09 12:32:09 +0200

    graph library test2

--- app/src/main/java/com/example/amitwati/athleticsapp/MainActivity.java
+++ app/src/main/java/com/example/amitwati/athleticsapp/MainActivity.java
@@ -1,45 +1,66 @@
 package com.example.amitwati.athleticsapp;
 
+
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import org.eazegraph.lib.charts.PieChart;
-import org.eazegraph.lib.charts.ValueLineChart;
-import org.eazegraph.lib.models.PieModel;
-import org.eazegraph.lib.models.ValueLinePoint;
-import org.eazegraph.lib.models.ValueLineSeries;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    BarChart chart ;
+    ArrayList<BarEntry> BARENTRY ;
+    ArrayList<String> BarEntryLabels ;
+    BarDataSet Bardataset ;
+    BarData BARDATA ;
+    static String[] month = {"January","February",
+            "March","April","May","June","July","August",
+            "September","October","November","December"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ValueLineChart mCubicValueLineChart = (ValueLineChart) findViewById(R.id.cubiclinechart);
+        //bind view to object
+        chart = (BarChart) findViewById(R.id.chart);
 
-        ValueLineSeries series = new ValueLineSeries();
-        series.setColor(Color.RED);
+        //value list
+        BARENTRY = new ArrayList<>();
 
-        for(int i=-10;i<50;i++) {
-            series.addPoint(new ValueLinePoint(String.valueOf(i), i*i*i));
+        //title list
+        BarEntryLabels = new ArrayList<String>();
+
+        for(int i=0;i<month.length;i++){
+            //add value to the column
+            BARENTRY.add(new BarEntry((new Random()).nextFloat() + (new Random()).nextInt(100), i));
+
+            //add title to the column
+            BarEntryLabels.add(month[i]);
         }
 
+        //config the visual of the data set
+        Bardataset = new BarDataSet(BARENTRY, "scores");
+        Bardataset.setValueTextSize(10f);
+        Bardataset.setColors(ColorTemplate.JOYFUL_COLORS);
+        Bardataset.setStackLabels(month);
+        BARDATA = new BarData(BarEntryLabels, Bardataset);
 
-        mCubicValueLineChart.addSeries(series);
-        mCubicValueLineChart.startAnimation();
-
-        PieChart mPieChart = (PieChart) findViewById(R.id.piechart);
-
-        mPieChart.addPieSlice(new PieModel("Freetime", 15, Color.parseColor("#FE6DA8")));
-        mPieChart.addPieSlice(new PieModel("Sleep", 25, Color.parseColor("#56B7F1")));
-        mPieChart.addPieSlice(new PieModel("Work", 35, Color.parseColor("#CDA67F")));
-        mPieChart.addPieSlice(new PieModel("Eating", 9, Color.parseColor("#FED70E")));
-
-        mPieChart.startAnimation();
-
-
+        //set the data to the chart
+        chart.setData(BARDATA);
+        chart.animateY(3000);
 
     }
+
+
+
+
+
 }
