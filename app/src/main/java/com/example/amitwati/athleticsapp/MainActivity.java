27df062de2d75ebe3b5728c458b4146367aee8c2
Amitwati <Amitwati69@gmail.com>
2017-11-11 14:12:45 +0200

    added new dialog

--- app/src/main/java/com/example/amitwati/athleticsapp/MainActivity.java
+++ app/src/main/java/com/example/amitwati/athleticsapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.amitwati.athleticsapp;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            (getWindow()).setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimaryDark_GREEN));
+        }
 
         //set the toolbar
         Toolbar myToolbar = findViewById(R.id.main_toolbar);
--- app/src/main/java/com/example/amitwati/athleticsapp/MenuActivity.java
+++ app/src/main/java/com/example/amitwati/athleticsapp/MenuActivity.java
@@ -1,15 +1,25 @@
 package com.example.amitwati.athleticsapp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
@@ -29,8 +39,8 @@ public class MenuActivity extends AppCompatActivity {
         myToolbar.setTitle("אתלטי הנגב");
         setSupportActionBar(myToolbar);
 
+        //set back button on the toolbar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,6 +48,7 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+        //set the buttons
         ListView lv = (ListView)findViewById(R.id.menu_list);
         ArrayList<Button> a = new ArrayList<>();
 
@@ -47,7 +58,36 @@ public class MenuActivity extends AppCompatActivity {
         AdapterMenu menu = new AdapterMenu(this,btns,"RED");
         lv.setAdapter(menu);
 
+    }
 
+    //show the popup dialog
+    public void dialog(View view) {
+        //build the dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //inflate the main view for the dialog
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.list_dialog_layout,null);
+
+        //change the view's params
+        ListView lv = (ListView)v.findViewById(R.id.list_view);
+        AdapterMenu adapterMenu = new AdapterMenu(this, new String[]{"עמית טואטי","עומרי אבידן","ירדן צ'רי","אביב אדר","דני ברומברג","אדיר אבוחצירה","מייקל ג'קסון","אלן פריד",},"BLUE");
+        lv.setAdapter(adapterMenu);
+
+        //set the height of the listview dynamically
+        //setListViewHeightBasedOnChildren(lv);
+
+        //set the view to the dialog
+        builder.setView(v);
+
+        //build the alert dialog view
+        AlertDialog dialog = builder.create();
+
+        //remove the titlebar
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //show the dialog
+        dialog.show();
 
     }
 
