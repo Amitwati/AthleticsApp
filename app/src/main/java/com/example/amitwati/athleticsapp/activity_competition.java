16cd72d1fb95d85dc937f38d3f261f07b6533bf9
Amitwati <Amitwati69@gmail.com>
2017-11-14 23:07:04 +0200

    color update and activity changes

--- app/src/main/java/com/example/amitwati/athleticsapp/activity_competition.java
+++ app/src/main/java/com/example/amitwati/athleticsapp/activity_competition.java
@@ -1,7 +1,10 @@
 package com.example.amitwati.athleticsapp;
 
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -16,6 +19,24 @@ public class activity_competition extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_competition);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            (getWindow()).setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimaryDark_YELLOW));
+        }
+        //set the toolbar
+        Toolbar myToolbar = findViewById(R.id.competition_toolbar);
+        myToolbar.setTitle("תחרויות");
+        setSupportActionBar(myToolbar);
+
+        //set back button on the toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
         String[] buttons = new String[]{"שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","תוצאות תחרויות","תוצאות מדידות"};
         ListView lv = (ListView)findViewById(R.id.ListView);
         ArrayList<View.OnClickListener> listeners = new ArrayList<>();
