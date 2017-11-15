735972ded4a83757176c9bba100f03a19e0491f6
Omri <omriavidan0402hn@gmail.com>
2017-11-15 14:16:38 +0300

    added dialog for adding a competition

--- app/src/main/java/com/example/amitwati/athleticsapp/activity_competition.java
+++ app/src/main/java/com/example/amitwati/athleticsapp/activity_competition.java
@@ -1,12 +1,18 @@
 package com.example.amitwati.athleticsapp;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -36,13 +42,7 @@ public class activity_competition extends AppCompatActivity {
                 onBackPressed();
             }
         });
-
-        String[] buttons = new String[]{"שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","תוצאות תחרויות","תוצאות מדידות"};
-        ListView lv = (ListView)findViewById(R.id.ListView);
-        ArrayList<View.OnClickListener> listeners = new ArrayList<>();
-        AdapterMenu adapterMenu = new AdapterMenu(this,buttons,null,"YELLOW");
-        lv.setAdapter(adapterMenu);
-
+        final ArrayList<String> buttons = new ArrayList<>();
         final TextView Year=(TextView)findViewById(R.id.YearView);
         Year.setText(Integer.toString(CurrYear));
         Button LeftButton=(Button)findViewById(R.id.Lbutton);
@@ -60,6 +60,54 @@ public class activity_competition extends AppCompatActivity {
             public void onClick(View view) {
                 Year.setText(Integer.toString(CurrYear+1));
                 CurrYear+=1;
+            }
+        });
+        final Context c = this;
+        Button AddBtn=(Button)findViewById(R.id.AddButton);
+        AddBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(c);
+                LayoutInflater inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View v = inflater.inflate(R.layout.add_comp_dialog,null);
+                Button ABtn=(Button)v.findViewById(R.id.btn_item);
+                final EditText Name=(EditText)v.findViewById(R.id.Comp_Text);
+                final EditText Date=(EditText)v.findViewById(R.id.Comp_Date);
+                builder.setView(v);
+
+                //build the alert dialog view
+                final AlertDialog dialog = builder.create();
+
+                //remove the titlebar
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+                //show the dialog
+                dialog.show();
+
+                //set the dismiss btn
+                ImageView back_btn = v.findViewById(R.id.back_btn);
+                back_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+                ABtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        buttons.add(Name.getText().toString()+" "+Date.getText().toString());
+                        String[] SButtons = new String[buttons.size()];
+                        SButtons = buttons.toArray(SButtons);
+                        ListView lv = (ListView)findViewById(R.id.ListView);
+                        ArrayList<View.OnClickListener> listeners = new ArrayList<>();
+                        AdapterMenu adapterMenu = new AdapterMenu(c, SButtons,null,"YELLOW");
+                        lv.setAdapter(adapterMenu);
+                        dialog.dismiss();
+                    }
+                });
+                //String[] buttons = new String[]{"שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","שיאים אישיים","תוצאות תחרויות","תוצאות מדידות"};
+
             }
         });
     }
